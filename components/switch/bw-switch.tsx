a2c8f5d8f19d44192cb05beb031b060bdae8a9bc
박지수 <jisupark2000@gmail.com>
2023-07-12 17:48:25 +0900

    Feat 흑백 스위치 컴포넌트 완성

---
+++ components/switch/bw-switch.tsx
@@ -0,0 +1,35 @@
+import { StoneColor } from '@/libs/domain/baduk/baduk';
+import { cls } from '@/utils/cls';
+import React from 'react';
+import styles from './bwSwitch.module.css';
+
+interface Props {
+  mode: StoneColor;
+  toggleFn?: () => void;
+}
+
+const BwSwitch: React.FC<Props> = ({ mode, toggleFn }) => {
+  return (
+    <div
+      className={cls(
+        'w-100 box-border rounded-full p-4 flex relative cursor-pointer select-none',
+        mode === 'BLACK' ? styles['color-black'] : styles['color-white'],
+      )}
+      onClick={toggleFn}
+    >
+      <div
+        className={cls(
+          'w-24 h-24 rounded-full shrink-0 absolute',
+          mode === 'BLACK' ? styles['move-black'] : styles['move-white'],
+        )}
+      ></div>
+      {mode === 'BLACK' && <div className='w-24' />}
+      <div className={cls('flex justify-center items-center w-full font-bold text-[16px]')}>
+        {mode === 'BLACK' ? 'black' : 'white'}
+      </div>
+      {mode === 'WHITE' && <div className='w-24' />}
+    </div>
+  );
+};
+
+export default BwSwitch;
