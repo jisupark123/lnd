f43dda562e13f67b28f88ffb506dbafa4bc83905
박지수 <jisupark2000@gmail.com>
2023-07-12 17:46:46 +0900

    Feat level-bar 컴포넌트 완성

---
+++ components/level-bar.tsx
@@ -0,0 +1,20 @@
+import { Level, levelFromLevelScore } from '@/libs/domain/level';
+import { cls } from '@/utils/cls';
+import React from 'react';
+
+interface Props {
+  width: number;
+  height: number;
+  levelScore: number;
+}
+
+const LevelBar: React.FC<Props> = ({ width, height, levelScore }) => {
+  const { level, grade } = levelFromLevelScore(levelScore);
+  return (
+    <div className='bg-bg_1 rounded-full overflow-hidden' style={{ width: `${width}px`, height: `${height}px` }}>
+      <div className={`bg-${level}`} style={{ width: `${grade * 20}%`, height: '100%' }} />
+    </div>
+  );
+};
+
+export default LevelBar;
