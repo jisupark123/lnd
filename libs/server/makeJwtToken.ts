f4f3b8b644a2eaa002075668996d7e7e5a8f5979
박지수 <jisupark2000@gmail.com>
2023-07-14 16:57:55 +0900

    Feat withHandler에 jwt 인증 로직 구현

--- libs/server/makeJwtToken.ts
+++ libs/server/makeJwtToken.ts
@@ -1,7 +1,7 @@
 import { sign } from 'jsonwebtoken';
 import { JwtExpiresInType } from '../domain/jwt';
 
-export function makeJwtToken(payload: string | object, privateKey: string, expiresIn: JwtExpiresInType) {
-  const jwtToken = sign(payload, privateKey, { expiresIn });
+export default function makeJwtToken(payload: string | object, expiresIn: JwtExpiresInType) {
+  const jwtToken = sign(payload, process.env.JWT_PRIVATE_KEY as string, { expiresIn });
   return jwtToken;
 }
