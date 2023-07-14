3c01eb9ea77d6b70caf63e0f976038ffe6089552
박지수 <jisupark2000@gmail.com>
2023-07-14 16:59:42 +0900

    Feat react-query, recoil 관련 설정 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { RecoilRoot } from 'recoil';
+
+declare global {
+  interface Window {
+    Kakao: {
+      init: (key: any) => void;
+      Auth: {
+        authorize: (options: { redirectUri: string }) => void;
+      };
+    };
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Script
+        src='https://developers.kakao.com/sdk/js/kakao.js'
+        onLoad={() => {
+          window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        }}
+      ></Script>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,71 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import CheckBox from '@/components/check-box/check-box';
+import LevelBar from '@/components/level-bar';
+import CheckBoxDropDown, { CheckBoxDropDownMenu } from '@/components/menu/check-box_drop-down';
+import DropDownMenu from '@/components/menu/drop-down-menu';
+import BackDrop from '@/components/modal/backdrop';
+import Switch from '@/components/switch/switch';
+import { StoneColor } from '@/libs/domain/baduk/baduk';
+import { Level } from '@/libs/domain/level';
+import AppResponseType from '@/types/appResponseType';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Home() {
+  const [checked, setChecked] = useState(true);
+  const [selectedLevel, setSelectedLevel] = useState<Level[]>(['bronze', 'silver']);
+  const toggleLevel = (prevSelectLevels: Level[], selectedLevel: Level) => {
+    if (prevSelectLevels.includes(selectedLevel)) {
+      return prevSelectLevels.filter((level) => level !== selectedLevel);
+    }
+    return [...prevSelectLevels, selectedLevel];
+  };
+  const dropDownMenus: CheckBoxDropDownMenu[] = [
+    {
+      selected: selectedLevel.includes('bronze'),
+      content: <div className='w-70 h-10 bg-bronze rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'bronze')),
+    },
+    {
+      selected: selectedLevel.includes('silver'),
+      content: <div className='w-70 h-10 bg-silver rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'silver')),
+    },
+    {
+      selected: selectedLevel.includes('gold'),
+      content: <div className='w-70 h-10 bg-gold rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'gold')),
+    },
+    {
+      selected: selectedLevel.includes('platinum'),
+      content: <div className='w-70 h-10 bg-platinum rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'platinum')),
+    },
+    {
+      selected: selectedLevel.includes('diamond'),
+      content: <div className='w-70 h-10 bg-diamond rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'diamond')),
+    },
+    {
+      selected: selectedLevel.includes('ruby'),
+      content: <div className='w-70 h-10 bg-ruby rounded-3' />,
+      onSelect: () => setSelectedLevel((prev) => toggleLevel(prev, 'ruby')),
+    },
+  ];
+
+  const fetchRequest = async () => {
+    const response = await axios.get<AppResponseType<{ ok: true }>>('/api/test');
+    console.log(response.data.result);
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div style={{}} className='flex justify-center items-center h-screen bg-bg_1'>
+      <DropDownMenu style={{ width: '150px' }} title='Lv'>
+        <CheckBoxDropDown style={{ width: '150px', top: '10px' }} menus={dropDownMenus} />
+      </DropDownMenu>
+      <CheckBox checked={checked} toggleFn={() => setChecked((prev) => !prev)} />
+      <Switch size='big' isOn={checked} toggleFn={() => setChecked((prev) => !prev)} />
+      <BackDrop>
+        <button onClick={fetchRequest}>눌러</button>
+      </BackDrop>
+    </div>
+  );
 }
