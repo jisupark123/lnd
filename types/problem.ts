e411b70ef310e9d833239f3b963fc2aabcda8e96
박지수 <jisupark2000@gmail.com>
2023-07-17 12:41:56 +0900

    Feat: 사활 종류 타입 추가

--- types/problem.ts
+++ types/problem.ts
@@ -1,5 +1,8 @@
 import { StoneColor } from '@/libs/domain/baduk/baduk';
 
+export type ProblemType = '사활' | '맥';
+export type ProblemResult = '살리는 문제' | '죽이는 문제' | '패';
+
 export interface ProblemFormat {
   dimensions: number; // 몇줄 바둑판
   firstTurn: StoneColor; // 흑선 or 백선
