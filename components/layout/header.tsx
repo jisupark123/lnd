9060d4269269463c08ab4f38af094a1430e0eaae
박지수 <jisupark2000@gmail.com>
2023-07-18 12:05:38 +0900

    Fix: header에 z-index 추가

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const Header = () => {
   const { user, isError: isUserError } = useUserQuery();
   return (
-    <div className='fixed w-full h-70 flex justify-center px-0 bg-white md:px-80'>
+    <div className='fixed w-full h-70 flex justify-center px-0 bg-white md:px-80 z-header'>
       <div className='flex justify-between items-center w-full px-20'>
         <Link href={'/'}>
           <Image src={'/imgs/logo_2.png'} alt='logo' width={180} height={0} />
